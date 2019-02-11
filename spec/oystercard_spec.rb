d4d36fa94c187171afc23d15779a57ca11bc55e6
Student <student@Admins-MacBook-Pro-3.local>
2019-02-11 15:08:31 +0000

    Closing #4 - adding top_method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,15 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
+
+  it 'adds money to balance' do
+    oystercard = Oystercard.new
+    #oystercard.top_up(5)
+    #oystercard.top_up(15)
+    #expect(oystercard.balance).to eq 20
+    expect{oystercard.top_up 15}.to change{oystercard.balance}.by 15
+  end
+
 end
 
 #NameError - Unitialized constant Oystercard
