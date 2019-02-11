598c7c62d698450a6bd29e46f4b1172bb457381f
Student <student@Admins-MacBook-Pro-3.local>
2019-02-11 14:52:54 +0000

    Closing #3 - passing first unit test and refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,14 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+
+  it 'checks that the cards balance is 0' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
+
 end
+
+#NameError - Unitialized constant Oystercard
+#./spec/oystercard_spec.rb:3
+#line 3
