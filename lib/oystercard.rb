287d1663796f71e2adbcd567e51dd61f9445863c
Student <student@Admins-MacBook-Pro-3.local>
2019-02-11 16:15:12 +0000

    Closing Step6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 
 class Oystercard
 attr_reader :balance
+MAXIMUM_BALANCE = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
-    @balance += amount
+      raise "Maximum Oystercard Balance Exceeded" if @balance + amount > MAXIMUM_BALANCE
+      @balance += amount
   end
-  
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,9 +13,19 @@ describe Oystercard do
     #oystercard.top_up(5)
     #oystercard.top_up(15)
     #expect(oystercard.balance).to eq 20
-    expect{oystercard.top_up 15}.to change{oystercard.balance}.by 15
+    expect{oystercard.top_up(15)}.to change{oystercard.balance}.by 15
   end
 
+  it 'has raises an error if balance exceeds maximum limit of £90' do
+  max_bal = Oystercard::MAXIMUM_BALANCE
+  oystercard = Oystercard.new
+  oystercard.top_up max_bal
+  #oystercard2 = Oystercard.new(85)
+  #oystercard1.top_up(3)
+  #oystercard2.top_up(15)
+  #expect{oystercard1.balance}.not_to raise_error.with_message("Maximum Oystercard Balance Exceeded")
+  expect{oystercard.top_up 1}.to raise_error "Maximum Oystercard Balance Exceeded"
+  end
 end
 
 #NameError - Unitialized constant Oystercard
