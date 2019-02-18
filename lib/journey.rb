aacd7fa0edf1ac38425e48a677ef89cf499e1e0d
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-18 11:51:07 +0000

    step 14

---
+++ lib/journey.rb
@@ -0,0 +1,9 @@
+require_relative 'oystercard'
+
+class Journey
+# move in journey?, touch_in, touch_out
+def initialize
+  
+end
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,7 @@
+require 'journey'
+
+describe Journey do
+let(:station) {double: station, zone}
+
+
+end
